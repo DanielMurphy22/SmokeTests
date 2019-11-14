47160f12bc575d336ec21330bf67b806edb64d48
DanielMurphy22 <daniel.murphy@stfc.ac.uk>
2019-11-14 16:44:15 +0000

    Add in Smoke Test 4.2 content

--- create_issues.py
+++ create_issues.py
@@ -30,18 +30,17 @@ def load_login_details(filename):
 # repository to add issue to)
 USERNAME, PASSWORD = load_login_details("login.txt")
 # will be loaded from login.txt:
-# USERNAME = 'keeeto'
+# USERNAME = 'DanielMurphy22'
 # PASSWORD = '******'
 # The repository to add this issue to
-REPO_NAME = 'mantid'
-REPO_OWNER = 'mantidproject'
+REPO_NAME = 'SmokeTests'
+REPO_OWNER = 'DanielMurphy22'
 
 gh = Github(USERNAME, PASSWORD)
 repo = gh.get_user(REPO_OWNER).get_repo(REPO_NAME)
 assignees = repo.get_assignees()
 
-#Lookup milestone sting to get number
-milestone = "Release 4.2"
+milestone = "Release 4.3"
 gh_milestone = None
 for loop_milestone in repo.get_milestones():
     if loop_milestone.title == milestone:
@@ -63,22 +62,19 @@ print( "Labels", gh_labels)
 
 
 body_text = '''
-You have been assigned unscripted testing. The hope is to catch as many problems with the code before release, so it would be great if you can take some time to give a serious test to your assigned area. Thank you!!
 
-The general guide to unscripted testing:
+Before testing, Edit the Smoke Testing Umbrella Issue to add your name beside the OS you are running this test on.
+Then tick the adjacent tick box once testing is complete.
 
-* The tests must be performed on the installer versions of the final release candidate. Not on local compiled code.
-* Serious errors involving loss of functionality, crashes etc. should be raised
-as issues with the current release as a milestone and an email sent to the project manager immediately.
-* Minor and cosmetic issues should be raised as issues against the forthcoming
-releases.
-* First try things that should work, then try to break Mantid, e.g. entering invalid values, unexpected characters etc.
-* Don't spend more than a few hours on the testing as fatigue will kick in.
-* If you find errors in the documentation, please correct them.
-* Comment against this ticket the OS environment you are testing against.
-* Close the this issue once you are done.
+Please run these tests on the compiled package of Mantid; **not a locally compiled version**.
+Test MantidPlot AND Workbench!
+
+Comment below with the OS you tested on
+and any issues you came across.
+
+If you have any questions please contact the creator of this issue.
+
 '''
-
 print( "\nLoading Issues")
 df = pd.read_excel("issue_template.xlsx","issues")
 
@@ -86,18 +82,11 @@ print( "\nCreating Issues")
 for index, row in df.iterrows():
     title =  row['Title']
     additional_body = row['Additional Body Text'] 
-    assignee =  row['Assignee']
     gh_assignee = None
-    if pd.notnull(assignee):
-        for loop_assignee in assignees:
-            if loop_assignee.login == assignee:
-                gh_assignee = assignee
-        if gh_assignee is None:
-            print( "could not find gh assignee for ", assignee, ". Continuing without assignment.")
-
     my_body = body_text
     if pd.notnull(additional_body):
-        my_body += "\n\n### Specific Notes:\n\n" + additional_body
+        my_body += "\n\n## Checklist/directions\n\n" + additional_body
     print (title,gh_assignee,gh_milestone,gh_labels)
-    issue = repo.create_issue(title, my_body, gh_assignee, gh_milestone, gh_labels) #COMMENT THIS OUT TO TEST BEFORE MAKING ISSUES
+#    issue = repo.create_issue(title, my_body, gh_assignee, gh_milestone, gh_labels) #COMMENT THIS OUT TO TEST BEFORE MAKING ISSUES
     print(issue.number, issue.title)
+
